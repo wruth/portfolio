ffa4f51aa3e999d599f278671a34cc7838ef631b
Ward Ruth <wruth@sonic.net>
2014-07-07 15:55:20 -0700

    initial commit project setup

---
+++ app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global Portfolio, $*/
+
+
+window.Portfolio = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    Portfolio.init();
+});
